24bca2f18f2c22f27263749ad225a7d79c34b09c
igCircle <df314376@126.com>
2025-08-12 20:47:39 +0800

    feat: 403 404 页面自定义

---
+++ src/pages/403.tsx
@@ -0,0 +1,17 @@
+import { history } from '@umijs/max';
+import { Button, Result } from 'antd';
+import React from 'react';
+const ForbiddenComponent: React.FC = () => (
+  <Result
+    status="403"
+    title="403"
+    subTitle="对不起，你无权访问该页面"
+    extra={
+      <Button type="primary" onClick={() => history.push('/')}>
+        返回首页
+      </Button>
+    }
+  />
+);
+
+export default ForbiddenComponent;
